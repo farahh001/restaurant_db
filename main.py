2a5f7c0c31d596ae9b45e37ba5777187d58a991b
sulta <fsultan001@citymail.cuny.edu>
2022-04-08 01:39:27 -0400

    first commit db added

---
+++ main.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template
+
+from flask_mysqldb import MySQL
+import mysql.connector
+
+app = Flask(__name__)
+#add database
+app.config['MYSQL_HOST'] ='localhost'
+app.config['MYSQL_USER'] ='root'
+app.config['MYSQL_PASSWORD'] ='f1234'
+app.config['MYSQL_DB'] ='project-1-336'
+#
+mysql =MySQL(app)
+
+# mydb=mysql.connector.connect(host="127.0.0.1",port="3307",user="root",passwd="farah1234",database="project-1-336")
+# mycursor=mydb.connection.cursor()
+# mycursor.execute("SELECT * FROM restaurant")
+
+
+
+
+
+@app.route('/')
+def index():
+    cur =mysql.connection.cursor()
+    cur.execute("SELECT * FROM address")
+    fetchdata =cur.fetchall()
+    cur.close()
+
+    return render_template("index.html",data =fetchdata)
+
+if __name__ == '__main__':
+    app.run(debug=True)
