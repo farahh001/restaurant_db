76cfd56c22fd3b0603fcd9cd3943c331c32d18f8
asamiul101 <58337911+samiularnob12@users.noreply.github.com>
2022-04-21 13:14:09 -0400

    login
    i made the login page

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 #add database
 app.config['MYSQL_HOST'] ='localhost'
 app.config['MYSQL_USER'] ='root'
-app.config['MYSQL_PASSWORD'] ='f1234'
+app.config['MYSQL_PASSWORD'] =''
 app.config['MYSQL_DB'] ='project-1-336'
 #
 mysql =MySQL(app)
