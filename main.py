2c43aaef8af394d531011ca60ab3597af075d8a6
alantepox98 <alan.tepoxtecatl@gmail.com>
2022-04-28 12:16:57 -0400

    changes to files

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 #add database
 app.config['MYSQL_HOST'] ='localhost'
 app.config['MYSQL_USER'] ='root'
-app.config['MYSQL_PASSWORD'] =''
+app.config['MYSQL_PASSWORD'] ='2041'
 app.config['MYSQL_DB'] ='project-1-336'
 #
 mysql =MySQL(app)
@@ -23,7 +23,7 @@ mysql =MySQL(app)
 @app.route('/')
 def index():
     cur =mysql.connection.cursor()
-    cur.execute("SELECT * FROM address")
+    cur.execute("SELECT * FROM address WHERE address_id <11")
     fetchdata =cur.fetchall()
     cur.close()
 
@@ -32,6 +32,14 @@ def index():
 @app.route('/registration/')
 def registration():
     return render_template("registration.html")
+
+@app.route('/login/')
+def login():
+    return render_template("login.html")
+
+@app.route('/about/')
+def about():
+    return render_template("about.html")
 
 
 if __name__ == '__main__':
